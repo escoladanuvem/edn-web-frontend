1e1ee70f4a891fea6ee0cae1383dcbca993d7ea8
Gabriel Martini <gabrielb.martini@gmail.com>
2020-12-28 16:23:14 -0300

    feat(Landing Page) Started landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,6 +1,21 @@
+import '../styles/pages/landing.css'
+import Illustration from '../images/landing-illustration.svg'
+
 function Landing() {
     return (
-        <p>Landing Content</p>
+        <div id='page-landing'>
+            <div className='content-wrapper'>
+                <main>
+                    <h1>
+                        Precisando encontrar profissionais de cloud?
+                    </h1>
+                    <h2>
+                        Preparando e conectando profissionais de nuvem para o mundo
+                    </h2>
+                </main>
+                <img src={Illustration} alt='ed' />
+            </div>
+        </div>
     )
 }
 
