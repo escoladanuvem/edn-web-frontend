37a058e2dd6f252f6b745b1c7605945b6bb546f1
Gabriel Martini <gabrielb.martini@gmail.com>
2021-01-03 02:20:26 -0300

    feat(Footer) Added Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+import Box from '@material-ui/core/Box';
+
+
+function Footer() {
+    return (
+        <Box mt={5}>
+            <Typography variant="body2" color="textSecondary" align="center">
+                {'© Todos os direitos reservados - '}
+                <Link color="inherit" href="https://www.escoladanuvem.com.br/">
+                    Escola da Nuvem
+                </Link>
+            </Typography>
+        </Box>
+    );
+}
+
+export default Footer;
